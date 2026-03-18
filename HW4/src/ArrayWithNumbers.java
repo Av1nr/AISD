9df1f3cb7db4727746ad41c0f541d13686e601d9
Av1nr <askarsteam5501@gmail.com>
2026-03-18 19:18:15 +0300

    ArrayWithNumbers.java

--- HW4/src/ArrayWithNumbers.java
+++ HW4/src/ArrayWithNumbers.java
@@ -11,22 +11,8 @@ class ArrayWithNumbers {
             System.out.print("Введите элемент на [" + i + "] месте: ");
             array[i] = enter.nextInt();
         }
-        enter.close();
-        if(!have0(array)){
-            System.out.println("Missing number is 0");
-        }
-        else{
-            System.out.print("Missing number is: ");
-            //Искал как сделать sum без использования переменных и вспомнил , что начали stream и lambda function
-            System.out.println((l)*(l+1)/2 - Arrays.stream(array).sum());
-        }
-    }
-    static boolean have0(int[] array){
-        for (int element : array) {
-            if (element == 0) {
-                return true;
-            }
-        }
-        return false;
+        System.out.print("Missing number is: ");
+        //Искал как сделать sum без использования переменных и вспомнил , что начали stream и lambda function
+        System.out.println((l)*(l+1)/2 - Arrays.stream(array).sum());
     }
 }
