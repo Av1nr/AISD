cbdaeb14c13dbfc48a57069030914d868ed0f8b7
Av1nr <askarsteam5501@gmail.com>
2026-03-18 19:03:45 +0300

    Add files via upload

---
+++ untitled/src/ArrayWithNumbers.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class ArrayWithNumbers {
+    static void main() {
+        Scanner enter = new Scanner(System.in);
+        System.out.print("Длина массива: ");
+        int l = enter.nextInt();
+        int[] array = new int[l];
+        for(int i = 0 ; i < l; i++){
+            System.out.print("Введите элемент на [" + i + "] месте: ");
+            array[i] = enter.nextInt();
+        }
+        if(!have0(array)){
+            System.out.println("Missing number is 0");
+        }
+        else{
+            System.out.print("Missing number is: ");
+            //Искал как сделать sum без использования переменных и вспомнил , что начали stream и lambda function
+            System.out.println((l)*(l+1)/2 - Arrays.stream(array).sum());
+        }
+    }
+    static boolean have0(int[] array){
+        for (int element : array) {
+            if (element == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ untitled/src/CycleShift.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class CycleShift {
+    static void main() {
+        Scanner enter = new Scanner(System.in);
+        System.out.print("Введите стартовое слово: ");
+        String start = enter.nextLine();
+        System.out.print("Введите слово , которое проверяется на циклический сдвиг: ");
+        String get = enter.nextLine();
+        if(start.equals(get)){
+            System.out.println("They are the same");
+        }
+        else if(start.length() != get.length()){
+            System.out.println("They have different length");
+        }
+        else if((start + start).contains(get)){
+            System.out.println("Cycle Shift");
+        }
+        else{
+            System.out.println("They are not the same and  there is no cycle shift");
+        }
+    }
+}
